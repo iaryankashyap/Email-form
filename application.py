77cc27c6909d3402f2bde311f4087e79933b8379
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-07-17 13:16:39 +0530

    Final

--- application.py
+++ application.py
@@ -5,6 +5,28 @@ from flask import Flask, flash, request, redirect, render_template
 
 app = Flask(__name__)
 
+def getusers():
+    sqcon = sq.connect(host='iaryankashyap.mysql.pythonanywhere-services.com', database='iaryankashyap$users',
+                       user='iaryankashyap', password='rootrootroot')
+    cursor = sqcon.cursor()
+    users = []
+    cursor.execute("SELECT * FROM users")
+    for i in cursor:
+        users.append(i[0])
+    sqcon.close()
+    return users
+
+def usercoun():
+    sqcon = sq.connect(host='iaryankashyap.mysql.pythonanywhere-services.com', database='iaryankashyap$users',
+                       user='iaryankashyap', password='rootrootroot')
+    cursor = sqcon.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users")
+    for i in cursor:
+        p = i[0]
+    usercount = p
+    sqcon.close()
+    return usercount
+
 def regemail(email):
     sqcon = sq.connect(host='iaryankashyap.mysql.pythonanywhere-services.com', database='iaryankashyap$users',
                        user='iaryankashyap', password='rootrootroot')
@@ -27,7 +49,8 @@ def validate():
     if request.method == "POST":
         key = request.form.get("key")
         if key=="superuser12345":
-            return render_template('dashboard.html')
+            ems= getusers()
+            return render_template('dashboard.html',users=ems,count=usercoun())
         else:
             return render_template('wrong.html')
 
@@ -36,9 +59,8 @@ def subem():
     if request.method == "POST":
         email = request.form.get("email")
         regemail(email)
-        return render_template('dashboard.html')
-    else:
-        return render_template('wrong.html')
+        return render_template('thankyou.html')
+
 '''
 if __name__ == "__main__":
     app.run(debug=True)
