fd2c26937553a2ab01c21e70ba6dab2f9e03f144
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-07-17 12:58:36 +0530

    Changed

--- application.py
+++ application.py
@@ -5,6 +5,15 @@ from flask import Flask, flash, request, redirect, render_template
 
 app = Flask(__name__)
 
+def regemail(email):
+    sqcon = sq.connect(host='iaryankashyap.mysql.pythonanywhere-services.com', database='iaryankashyap$users',
+                       user='iaryankashyap', password='rootrootroot')
+    cursor = sqcon.cursor()
+    query = "INSERT INTO users(email) VALUES('"+email+"')"
+    cursor.execute(query)
+    sqcon.commit()
+    sqcon.close()
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -22,5 +31,15 @@ def validate():
         else:
             return render_template('wrong.html')
 
+app.route('/submit', methods=["GET", "POST"])
+def subem():
+    if request.method == "POST":
+        email = request.form.get("email")
+        regemail(email)
+        return render_template('dashboard.html')
+    else:
+        return render_template('wrong.html')
+'''
 if __name__ == "__main__":
     app.run(debug=True)
+    '''
